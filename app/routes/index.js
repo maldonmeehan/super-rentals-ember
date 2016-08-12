17036fab1d47b63043906ef227757ef08fe5f59c
Maldon <maldonmeehan@gmail.com>
2016-08-12 15:22:08 -0700

    add working save and delete components

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,8 +4,12 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('rental');
   },
-
   actions: {
+    save3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
     destroyRental(rental) {
       rental.destroyRecord();
       this.transitionTo('index');
@@ -13,31 +17,31 @@ export default Ember.Route.extend({
   }
 });
 
-var rentals = [{
-  id: 1,
-  owner: "Veruca Salt",
-  city: "San Francisco",
-  type: "Estate",
-  bedrooms: 15,
-  image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
-}, {
-  id: 2,
-  owner: "Mike TV",
-  city: "Seattle",
-  type: "Condo",
-  bedrooms: 1,
-  image: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg"
-}, {
-  id: 3,
-  owner: "Violet Beauregarde",
-  city: "Portland",
-  type: "Apartment",
-  bedrooms: 3,
-  image: "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg"
-}];
-
-export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('rental');
-  },
-});
+// var rentals = [{
+//   id: 1,
+//   owner: "Veruca Salt",
+//   city: "San Francisco",
+//   type: "Estate",
+//   bedrooms: 15,
+//   image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
+// }, {
+//   id: 2,
+//   owner: "Mike TV",
+//   city: "Seattle",
+//   type: "Condo",
+//   bedrooms: 1,
+//   image: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg"
+// }, {
+//   id: 3,
+//   owner: "Violet Beauregarde",
+//   city: "Portland",
+//   type: "Apartment",
+//   bedrooms: 3,
+//   image: "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg"
+// }];
+//
+// export default Ember.Route.extend({
+//   model() {
+//     return this.store.findAll('rental');
+//   },
+// });
